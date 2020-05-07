c98e69293b47e4e34b430f0618c0c489e02c105d
Ifteear Hossain <iteear1999@gmail.com>
2020-05-08 01:24:14 +0600

    Change function name

--- hello.php
+++ hello.php
@@ -7,14 +7,16 @@
     echo "$fname Refsnes.<br>";
 }
 
+function faltu($atom) {
+    echo "$Name Gorila.<br>"
+
     familyName("Jani");
     familyName("Hege");
     familyName("Stale");
     familyName("Kai Jim");
     familyName("Borge");
 
-     function SystemName($System) {
-    echo "$Name Gorila.<br>";
+     ;
 ?>
 
 </body>
